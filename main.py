87eab785969831dc06b6c411229670c5ae99dd47
MayankVaswani100 <coder.mayan@gmail.com>
2021-06-06 19:20:52 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+nr_letters = int(input(f"How many letters would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+for letter in range(1, nr_letters + 1):
+    randl = random.choice(letters)
+    password.append(randl)
+
+for number in range(1, nr_numbers + 1):
+    randn = random.choice(numbers)
+    password.append(randn)
+
+for symb in range(1, nr_symbols + 1):
+    rands = random.choice(symbols)
+    password.append(rands)
+
+random.shuffle(password)
+final_password = ""
+for i in password:
+    final_password += i
+print(final_password)
